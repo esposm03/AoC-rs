f9a62785453ee0e820160fca90cf5ddb26b93c8d
esposm03 <36164633+esposm03@users.noreply.github.com>
2022-11-11 22:35:15 +0100

    feat(2021): day 5 part 1, 2

---
+++ src/solutions_2021/day5.rs
@@ -0,0 +1,124 @@
+use std::cmp::Ordering;
+
+use crate::SolutionType;
+
+pub fn day5(input: &str) -> SolutionType {
+    // First index is columns, second index is rows
+    let mut matrix: Box<[[u16; 1000]; 1000]> = Box::new([[0; 1000]; 1000]);
+
+    for line in input.lines() {
+        let mut iter = line.trim().split(',').map(|n| n.parse::<usize>().unwrap());
+        let mut col1 = iter.next().unwrap();
+        let mut row1 = iter.next().unwrap();
+        let mut col2 = iter.next().unwrap();
+        let mut row2 = iter.next().unwrap();
+
+        if row1 > row2 {
+            let temp = row1;
+            row1 = row2;
+            row2 = temp;
+        }
+        if col1 > col2 {
+            let temp = col1;
+            col1 = col2;
+            col2 = temp;
+        }
+
+        if col1 == col2 {
+            for row in row1..=row2 {
+                matrix[col1][row] += 1;
+            }
+        } else if row1 == row2 {
+            for col in col1..=col2 {
+                matrix[col][row1] += 1;
+            }
+        }
+    }
+
+    let mut sum = 0;
+    for col in matrix.iter() {
+        for row in col {
+            if *row >= 2 {
+                sum += 1;
+            }
+        }
+    }
+    SolutionType::Int(sum as i64)
+}
+
+pub fn day5_part2(input: &str) -> SolutionType {
+    // First index is columns, second index is rows
+    let mut matrix: Box<[[u16; 1000]; 1000]> = Box::new([[0; 1000]; 1000]);
+
+    for line in input.lines() {
+        let mut iter = line.trim().split(',').map(|n| n.parse::<usize>().unwrap());
+        let mut col1 = iter.next().unwrap();
+        let mut row1 = iter.next().unwrap();
+        let mut col2 = iter.next().unwrap();
+        let mut row2 = iter.next().unwrap();
+
+        if row1 != row2 && col1 != col2 {
+            for i in 0..=col1.abs_diff(col2) {
+                let i = i as isize;
+                let (col_off, row_off) = match (col1.cmp(&col2), row1.cmp(&row2)) {
+                    (Ordering::Less, Ordering::Less) => (i, i),
+                    (Ordering::Less, Ordering::Greater) => (i, -i),
+                    (Ordering::Greater, Ordering::Less) => (-i, i),
+                    (Ordering::Greater, Ordering::Greater) => (-i, -i),
+                    _ => unreachable!(),
+                };
+
+                matrix[(col1 as isize + col_off) as usize][(row1 as isize + row_off) as usize] += 1;
+            }
+        }
+
+        if row1 > row2 {
+            let temp = row1;
+            row1 = row2;
+            row2 = temp;
+        }
+        if col1 > col2 {
+            let temp = col1;
+            col1 = col2;
+            col2 = temp;
+        }
+
+        if col1 == col2 {
+            for row in row1..=row2 {
+                matrix[col1][row] += 1;
+            }
+        } else if row1 == row2 {
+            for col in col1..=col2 {
+                matrix[col][row1] += 1;
+            }
+        }
+    }
+
+    let mut sum = 0;
+    for col in matrix.iter() {
+        for row in col {
+            if *row >= 2 {
+                sum += 1;
+            }
+        }
+    }
+    SolutionType::Int(sum)
+}
+
+#[test]
+#[cfg(test)]
+fn test() {
+    let input = "0,9,5,9
+                8,0,0,8
+                9,4,3,4
+                2,2,2,1
+                7,0,7,4
+                6,4,2,0
+                0,9,2,9
+                3,4,1,4
+                0,0,8,8
+                5,5,8,2";
+
+    assert_eq!(day5(input), SolutionType::Int(5));
+    assert_eq!(day5_part2(input), SolutionType::Int(12));
+}
