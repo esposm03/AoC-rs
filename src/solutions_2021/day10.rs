d74bceaf57a3f8f0881b47aaab445853fab9a677
esposm03 <36164633+esposm03@users.noreply.github.com>
2022-11-12 19:57:14 +0100

    feat(2021): day 10 part 1, 2

---
+++ src/solutions_2021/day10.rs
@@ -0,0 +1,144 @@
+use crate::SolutionType;
+use Parens::*;
+
+pub fn day10(input: &str) -> SolutionType {
+    let mut score = 0;
+    for line in input.lines() {
+        let mut stack = vec![];
+        for char in line.trim().chars() {
+            match char {
+                '(' => stack.push(Round),
+                '[' => stack.push(Square),
+                '{' => stack.push(Curly),
+                '<' => stack.push(Angled),
+                ')' => {
+                    if let Some(Round) = stack.pop() {
+                    } else {
+                        score += 3;
+                        break;
+                    }
+                }
+                ']' => {
+                    if let Some(Square) = stack.pop() {
+                    } else {
+                        score += 57;
+                        break;
+                    }
+                }
+                '}' => {
+                    if let Some(Curly) = stack.pop() {
+                    } else {
+                        score += 1197;
+                        break;
+                    }
+                }
+                '>' => {
+                    if let Some(Angled) = stack.pop() {
+                    } else {
+                        score += 25137;
+                        break;
+                    }
+                }
+                e => unreachable!("what is {e}?"),
+            }
+        }
+    }
+
+    SolutionType::Int(score)
+}
+
+pub fn day10_part2(input: &str) -> SolutionType {
+    let mut scores = vec![];
+
+    for line in input.lines() {
+        let mut corrupted = false;
+        let mut stack = vec![];
+
+        for char in line.trim().chars() {
+            match char {
+                '(' => stack.push(Round),
+                '[' => stack.push(Square),
+                '{' => stack.push(Curly),
+                '<' => stack.push(Angled),
+                ')' => match stack.pop() {
+                    Some(Round) => {}
+                    Some(_) => corrupted = true,
+                    None => break,
+                },
+                ']' => match stack.pop() {
+                    Some(Square) => {}
+                    Some(_) => corrupted = true,
+                    None => break,
+                },
+                '}' => match stack.pop() {
+                    Some(Curly) => {}
+                    Some(_) => corrupted = true,
+                    None => break,
+                },
+                '>' => match stack.pop() {
+                    Some(Angled) => {}
+                    Some(_) => corrupted = true,
+                    None => break,
+                },
+                e => unreachable!("what is {e}?"),
+            }
+        }
+
+        if corrupted {
+            continue;
+        }
+
+        stack.reverse();
+        if !stack.is_empty() {
+            let mut score = 0;
+            for leftover in stack {
+                score *= 5;
+                score += match leftover {
+                    Round => 1,
+                    Square => 2,
+                    Curly => 3,
+                    Angled => 4,
+                };
+            }
+            scores.push(score);
+        }
+    }
+
+    scores.sort();
+    SolutionType::Int(scores[scores.len() / 2])
+}
+
+#[derive(PartialEq, Eq)]
+enum Parens {
+    Round,
+    Square,
+    Curly,
+    Angled,
+}
+
+#[test]
+#[cfg(test)]
+fn test() {
+    let input = "[({(<(())[]>[[{[]{<()<>>
+        [(()[<>])]({[<{<<[]>>(
+        {([(<{}[<>[]}>{[]{[(<()>
+        (((({<>}<{<{<>}{[]{[]{}
+        [[<[([]))<([[{}[[()]]]
+        [{[{({}]{}}([{[{{{}}([]
+        {<[[]]>}<{[{[{[]{()[[[]
+        [<(<(<(<{}))><([]([]()
+        <{([([[(<>()){}]>(<<{{
+        <{([{{}}[<[[[<>{}]]]>[]]";
+
+    let input_incomplete = "
+    [({(<(())[]>[[{[]{<()<>>
+    [(()[<>])]({[<{<<[]>>(
+    (((({<>}<{<{<>}{[]{[]{}
+    {<[[]]>}<{[{[{[]{()[[[]
+    <{([{{}}[<[[[<>{}]]]>[]]
+";
+
+    assert_eq!(day10(input), SolutionType::Int(26397));
+    assert_eq!(day10_part2(input), SolutionType::Int(288957));
+    //assert_eq!(day10_part2(input), SolutionType::Int(288957));
+}
--- src/solutions_2021/mod.rs
+++ src/solutions_2021/mod.rs
@@ -3,6 +3,7 @@ use crate::{declare_solutions, SolutionType};
 declare_solutions!();
 
 mod day1;
+mod day10;
 mod day2;
 mod day3;
 mod day4;
@@ -12,7 +13,6 @@ mod day7;
 mod day8;
 mod day9;
 /*
-mod day10;
 mod day11;
 mod day12;
 mod day13;
@@ -20,6 +20,8 @@ mod day13;
 
 pub use day1::day1;
 pub use day1::day1_part2;
+pub use day10::day10;
+pub use day10::day10_part2;
 pub use day2::day2;
 pub use day2::day2_part2;
 pub use day3::day3;
@@ -37,8 +39,6 @@ pub use day8::day8_part2;
 pub use day9::day9;
 pub use day9::day9_part2;
 /*
-pub use day10::day10;
-pub use day10::day10_part2;
 pub use day11::day11;
 pub use day11::day11_part2;
 pub use day12::day12;
@@ -46,9 +46,6 @@ pub use day12::day12_part2;
 pub use day13::day13;
 */
 
-pub fn day10_part2(_: &str) -> SolutionType {
-    unimplemented!()
-}
 pub fn day11_part2(_: &str) -> SolutionType {
     unimplemented!()
 }
@@ -92,9 +89,6 @@ pub fn day24_part2(_: &str) -> SolutionType {
     unimplemented!()
 }
 pub fn day25_part2(_: &str) -> SolutionType {
-    unimplemented!()
-}
-pub fn day10(_: &str) -> SolutionType {
     unimplemented!()
 }
 pub fn day11(_: &str) -> SolutionType {
